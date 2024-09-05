708b9427a6d7bbd53a7127cac724b16009a9eafc
alex <alex@ubu04.localdomain>
2024-09-05 17:35:58 +0500

    hw-sdb 11-04.2

--- 11-04/consumer.py
+++ 11-04/consumer.py
@@ -2,14 +2,15 @@
 # coding=utf-8
 import pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+credentials = pika.PlainCredentials('test', 'test')
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost',5672,'/',credentials))
 channel = connection.channel()
-channel.queue_declare(queue='hello')
+channel.queue_declare(queue='zdraste')
 
 
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
 
 
-channel.basic_consume(callback, queue='hello', no_ack=True)
+channel.basic_consume('zdraste',callback,auto_ack=False)
 channel.start_consuming()
--- 11-04/producer.py
+++ 11-04/producer.py
@@ -2,8 +2,9 @@
 # coding=utf-8
 import pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+credentials = pika.PlainCredentials('test', 'test')
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost',5672,'/',credentials))
 channel = connection.channel()
-channel.queue_declare(queue='hello')
-channel.basic_publish(exchange='', routing_key='hello', body='Hello Netology!')
+channel.queue_declare(queue='zdraste')
+channel.basic_publish(exchange='', routing_key='zdraste', body='Privet Netology!')
 connection.close()
