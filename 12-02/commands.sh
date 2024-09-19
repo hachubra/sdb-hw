c3d78aa4a454fe28fa3a59a429a773196f8aca27
alex <alex@ubu04.localdomain>
2024-09-19 10:15:49 +0500

    sdb-hw 12-02.2

--- 12-02/commands.sh
+++ 12-02/commands.sh
@@ -1,2 +1,23 @@
-docker compose -d up
+docker compose up
 sudo dpkg -i /home/alex/Downloads/dbeaver-ce_24.2.0_amd64.deb 
+
+mkdir /home/alex/mysql/dbdata/dump
+cd /home/alex/mysql/dbdata/dump
+curl --output /home/alex/mysql/dbdata/dump/sakila-db.zip https://downloads.mysql.com/docs/sakila-db.zip 
+ls
+unzip sakila-db.zip
+ls
+
+
+docker exec -it mysql mysql -u sys_temp -p sys
+CREATE DATABASE sakila;
+exit
+ mysql -u sys_temp -p sakila < /var/lib/mysql/dump/sakila-db/sakila-schema.sql 
+ mysql -u sys_temp -p sakila < /var/lib/mysql/dump/sakila-db/sakila-data.sql 
+
+
+ #список всех таблиц
+ mysql -u sys_temp -p sakila
+
+ show tables;
+ 
